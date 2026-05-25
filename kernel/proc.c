30e5e92e553e646c37941ffc540765f53114c0e4
cxy0629 <1501379586@qq.com>
2026-05-26 00:18:54 +0800

    finish task1~3

--- kernel/proc.c
+++ kernel/proc.c
@@ -132,6 +132,16 @@ found:
     return 0;
   }
 
+  // 分配usyscall页
+#ifdef LAB_PGTBL
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->usyscall->pid = p->pid;
+#endif
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -160,6 +170,11 @@ freeproc(struct proc *p)
   p->trapframe = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
+#ifdef LAB_PGTBL
+  if(p->usyscall)
+    kfree((void*)p->usyscall);
+  p->usyscall = 0;
+#endif
   p->pagetable = 0;
   p->sz = 0;
   p->pid = 0;
@@ -202,7 +217,18 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // 映射usyscall页
+#ifdef LAB_PGTBL
+  if(mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->usyscall), PTE_U | PTE_R) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+#endif
+
   return pagetable;
+
 }
 
 // Free a process's page table, and free the
@@ -212,6 +238,9 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+#ifdef LAB_PGTBL
+  uvmunmap(pagetable, USYSCALL, 1, 0);
+#endif
   uvmfree(pagetable, sz);
 }
 
